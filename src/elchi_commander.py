04671f1ea63ab62fcd938e3fd8ba64b58529d508
Alex <alex.schmid@gmail.com>
2025-11-04 11:59:39 +0100

    Add functionality to the main script. So far, no logging.

--- src/elchi_commander.py
+++ src/elchi_commander.py
@@ -1,15 +1,68 @@
-# Rough idea of program structure
+import argparse
+from argparse import ArgumentError
+from pathlib import Path
+
+from platformdirs import user_config_dir
+
+from helpers.execute_action import (execute_massflow_action, execute_triggerbox_action,
+                                    execute_multiplexer_action, execute_temperature_action,
+                                    execute_blind_temperature_action)
+from helpers.exit import delayed_exit, report_sucess
+from helpers.file_load import load_config
+from helpers.validate import validate_config
+
 
 def main() -> None:
-    # parse command line argument
-    # validate it
+    print('Hi! This is ElchiCommander!')
 
-    # Read in the experiement definition file
-    # Validate if the file is valid
-    # Select the chosen action
+    parser = argparse.ArgumentParser(description='ElchiCommander is a CLI application for experiment control!'
+                                                 ' It can execute actions defined in a configuration file!',
+                                     exit_on_error=False)
+    parser.add_argument('action_id', type=int, help='The action to execute. Actions are defined in the config file.')
 
-    # try to connect devices
-    # execte the action
-    # log what you did
+    try:
+        args = parser.parse_args()
+    except ArgumentError as e:
+        delayed_exit(f'Invalid command line arguments, {e}')
+    else:
+        if args.action_id < 1:
+            delayed_exit('Invalid action id! Valid action ids are positive integers!')
+        else:
+            print(f'Action {args.action_id} requested!')
 
-    pass
+        config_dir = Path(user_config_dir('ElchiCommander', 'ElchWorks', roaming=True))
+        config_dir.mkdir(parents=True, exist_ok=True)
+
+        config = load_config(config_dir / 'config.yaml')
+        validate_config(config)
+
+        device_config = config.get('devices')
+        action_config = config.get('actions').get(args.action_id)
+        if action_config is None:
+            delayed_exit(f'Action with id {args.action_id} not found in config file!')
+        else:
+            print(f'Executing action {args.action_id}...')
+
+        match action_config['type']:
+            case 'gas_ctrl':
+                execute_massflow_action(action_config, device_config)
+            case 'triggerbox':
+                execute_triggerbox_action(action_config, device_config)
+            case 'multiplexer':
+                execute_multiplexer_action(action_config, device_config)
+            case 'set_temp':
+                execute_temperature_action(action_config, device_config)
+            case 'set_temp_blind':
+                execute_blind_temperature_action(action_config, device_config)
+            case _:
+                # This should never be reached as the config was validated before
+                delayed_exit(f'Invalid action type encountered: {action_config['type']}!')
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        delayed_exit(f'Un unexpected error occured: {e}')
+    else:
+        report_sucess()
